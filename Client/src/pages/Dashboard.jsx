2b94cf01d13f67c85b38970aff874215b43c8025
MeenaSharath <meenanairkcy131@gmail.com>
2025-06-09 20:58:21 +0530

    updated dashboard

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -68,7 +68,7 @@ function Dashboard() {
 
             {/* Cards */}
             <div>
-            {location.pathname === '/' && (
+            {location.pathname === '/e-commerce-project' && (
               <div>
               <div  className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
                 <h1 className=" fs-2 text-xl md:text-xl text-gray-800 dark:text-gray-100 font-bold">Dashboard</h1>
